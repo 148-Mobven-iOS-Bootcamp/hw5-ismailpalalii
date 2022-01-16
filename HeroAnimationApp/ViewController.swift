54747b93a99010c1c52d0f2699e06133027e3168
Ismail Palali <ismail.palali.pp@gmail.com>
2022-01-16 19:36:41 +0300

    Add TableView and Detail Screen

--- HeroAnimationApp/ViewController.swift
+++ HeroAnimationApp/ViewController.swift
@@ -8,13 +8,22 @@
 import UIKit
 import Hero
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+  
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Deneme"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 }
 
